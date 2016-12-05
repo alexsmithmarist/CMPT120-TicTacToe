542769332553722fef929caa679ea2fcb3473a42
alexsmithmarist <alex.smith1@marist.edu>
2016-12-04 20:13:18 -0500

    Added Olympic decorations, sound, images, and reset button.

--- tictactoe.js
+++ tictactoe.js
@@ -1,23 +1,34 @@
 /* Alex Smith
-November 10,2016
-Project 2 */
+December 4,2016
+Project 4 */
+
+var gold = document.getElementById("gold");
+var silver = document.getElementById("silver");
+var USAc = 0; // for standings
+var Canadac = 0; // for standings
+var cheer = new Audio("cheer.wav"); // crowd audio
+
+gold.innerHTML = "Gold Medal for USA! With " + USAc +" victories!" + "<img src='gold.png' alt='Gold medal' height='40' width='40'>";
+silver.innerHTML = "Silver Medal for Canada! With " +Canadac +" victories!" + "<img src='silver.jpg' alt='Silver medal' height='40' width='40'>";
 
 function changeplayer() 
 { 
-	// You need to add a statement here to find out who is currently playing the game, “X” or “O” by reading the paragraph “currentplayer” in tictactoe.html, and save it in a variable called player
+	// You need to add a statement here to find out who is currently playing the game, “USA” or “Canada” by reading the paragraph “currentplayer” in tictactoe.html, and save it in a variable called player
 	var player = document.getElementById("currentplayer").innerHTML;
+	var country = document.getElementById("country").innerHTML;
 
-	// Display “X” and “O” by turns
-	if(player =="X")
+	
+	if(country =="USA")
 	{
-		// The next user to play is “O” if the current user is “X”
-		document.getElementById("currentplayer").innerHTML = "O";
+		
+		document.getElementById("country").innerHTML = "Canada";
+		document.getElementById("currentplayer").innerHTML = "<img src = 'canadianflag.jpg' alt = 'Canada' height = '85' width = '85'>";
 	}
 	else
 	{
-		// Complete the else block
-		// The next user to play is “X” if the current user is “O”
-		document.getElementById("currentplayer").innerHTML = "X";
+		
+		document.getElementById("country").innerHTML = "USA";
+		document.getElementById("currentplayer").innerHTML = "<img src = 'americanflag.jpg' alt = 'USA' height = '85' width = '85'>";
 	}
 }
 
@@ -28,18 +39,26 @@ function set(idvalue)
 	
 	if(buttonclicked.innerHTML == ""||buttonclicked.innerHTML == null)
 	{
-	 // If the button is not clicked 
+		// If the button is not clicked 
 
 
 
-	// If the button is not clicked 
-	// Complete the function from here
-	// Add a statement to find out who is the current player, “X” or “O” by reading paragraph with id “currentplayer” in tictactoe.html
-	var readpara = document.getElementById("currentplayer").innerHTML;
+		// If the button is not clicked 
+		
+		var readpara = document.getElementById("currentplayer").innerHTML;
+		var country = document.getElementById("country").innerHTML;
+		
+		if(country == "USA")
+		{
+			buttonclicked.innerHTML = "<img src = 'americanflag.jpg' alt = 'USA' height = '85' width = '85'>";
+		}
+		else
+		{
+			buttonclicked.innerHTML = "<img src = 'canadianflag.jpg' alt = 'Canada' height = '85' width = '85'>";
+		}
 
-	// Add a statement to display the current player (“X” or “O”) on the button clicked (Hint: innerHTML) 
-	buttonclicked.innerHTML = readpara;
-	check_win();
+		// Add a statement to display the current player (“USA” or “Canada”) on the button clicked (Hint: innerHTML)
+		check_win();
 	}
 	else
 	{
@@ -52,51 +71,200 @@ function set(idvalue)
 function check_win() 
 { 
 	var player = document.getElementById("currentplayer").innerHTML;
+	var country = document.getElementById("country").innerHTML;
+	var head = document.getElementById("head"); //Flags of the currently winning country are displayed next to the title.
+	
 	
 	if(document.getElementById("0").innerHTML ==  document.getElementById("1").innerHTML && document.getElementById("1").innerHTML ==  document.getElementById("2").innerHTML &&document.getElementById("1").innerHTML == player)
 	{ 
-    	alert("You win! The winner is "+ player); 
+		cheer.play();
+		alert("You win! The winner is " + country);
+		if(country == "USA")
+		{
+			USAc = USAc + 1;
+		}
+		else
+		{
+			Canadac = Canadac + 1;
+		}
   	}
 	
 	else if(document.getElementById("3").innerHTML ==  document.getElementById("4").innerHTML && document.getElementById("4").innerHTML ==  document.getElementById("5").innerHTML &&document.getElementById("4").innerHTML == player)
 	{ 
-    	alert("You win! The winner is "+ player); 
+		cheer.play();
+		alert("You win! The winner is " + country);
+		if(country == "USA")
+		{
+			USAc = USAc + 1;
+		}
+		else
+		{
+			Canadac = Canadac + 1;
+		}
   	}
 	
 	else if(document.getElementById("6").innerHTML ==  document.getElementById("7").innerHTML && document.getElementById("7").innerHTML ==  document.getElementById("8").innerHTML &&document.getElementById("7").innerHTML == player)
 	{ 
-    	alert("You win! The winner is "+ player); 
+    	cheer.play();
+		alert("You win! The winner is " + country);
+		if(country == "USA")
+		{
+			USAc = USAc + 1;
+		}
+		else
+		{
+			Canadac = Canadac + 1;
+		}
   	}
 	
 	else if(document.getElementById("0").innerHTML ==  document.getElementById("3").innerHTML && document.getElementById("3").innerHTML ==  document.getElementById("6").innerHTML &&document.getElementById("3").innerHTML == player)
 	{ 
-    	alert("You win! The winner is "+ player); 
+    	cheer.play();
+		alert("You win! The winner is " + country);
+		if(country == "USA")
+		{
+			USAc = USAc + 1;
+		}
+		else
+		{
+			Canadac = Canadac + 1;
+		}
   	}
 	
 	else if(document.getElementById("1").innerHTML ==  document.getElementById("4").innerHTML && document.getElementById("4").innerHTML ==  document.getElementById("7").innerHTML &&document.getElementById("4").innerHTML == player)
 	{ 
-    	alert("You win! The winner is "+ player); 
+    	cheer.play();
+		alert("You win! The winner is " + country);
+		if(country == "USA")
+		{
+			USAc = USAc + 1;
+		}
+		else
+		{
+			Canadac = Canadac + 1;
+		}
   	}
 	
 	else if(document.getElementById("2").innerHTML ==  document.getElementById("5").innerHTML && document.getElementById("5").innerHTML ==  document.getElementById("8").innerHTML &&document.getElementById("5").innerHTML == player)
 	{ 
-    	alert("You win! The winner is "+ player); 
+    	cheer.play();
+		alert("You win! The winner is " + country);
+		if(country == "USA")
+		{
+			USAc = USAc + 1;
+		}
+		else
+		{
+			Canadac = Canadac + 1;
+		}
   	}
 	
 	else if(document.getElementById("0").innerHTML ==  document.getElementById("4").innerHTML && document.getElementById("4").innerHTML ==  document.getElementById("8").innerHTML &&document.getElementById("4").innerHTML == player)
 	{ 
-    	alert("You win! The winner is "+ player); 
+    	cheer.play();
+		alert("You win! The winner is " + country);
+		if(country == "USA")
+		{
+			USAc = USAc + 1;
+		}
+		else
+		{
+			Canadac = Canadac + 1;
+		}
   	}
 	
 	else if(document.getElementById("2").innerHTML ==  document.getElementById("4").innerHTML && document.getElementById("4").innerHTML ==  document.getElementById("6").innerHTML &&document.getElementById("4").innerHTML == player)
 	{ 
-    	alert("You win! The winner is "+ player); 
+    	cheer.play(); //Plays cheering crowd audio
+		alert("You win! The winner is " + country);
+		if(country == "USA")
+		{
+			USAc = USAc + 1;
+		}
+		else
+		{
+			Canadac = Canadac + 1;
+		}
   	}
 	
 	else
 	{
-		changeplayer(); // nobody wins, change player
+		changeplayer();
+	}
+	
+	if(USAc > Canadac) //Used to properly display standings
+	{
+		if(USAc > 1 && Canadac == 1)
+		{
+			gold.innerHTML = "Gold Medal for USA! With " + USAc +" victories!" + "<img src='gold.png' alt='Gold medal' height='40' width='40'>";
+			silver.innerHTML = "Silver Medal for Canada! With " +Canadac +" victory!" + "<img src='silver.jpg' alt='Silver medal' height='40' width='40'>";	
+		}
+		else if(USAc > 1 && Canadac > 1)
+		{
+			gold.innerHTML = "Gold Medal for USA! With " + USAc +" victories!" + "<img src='gold.png' alt='Gold medal' height='40' width='40'>";
+			silver.innerHTML = "Silver Medal for Canada! With " +Canadac +" victories!" + "<img src='silver.jpg' alt='Silver medal' height='40' width='40'>";	
+		}
+		else if(USAc == 1 && Canadac == 1)
+		{
+			gold.innerHTML = "Gold Medal for USA! With " + USAc +" victory!" + "<img src='gold.png' alt='Gold medal' height='40' width='40'>";
+			silver.innerHTML = "Silver Medal for Canada! With " +Canadac +" victory!" + "<img src='silver.jpg' alt='Silver medal' height='40' width='40'>";	
+		}
+		else if(USAc > 1 && Canadac == 0)
+		{
+			gold.innerHTML = "Gold Medal for USA! With " + USAc +" victories!" + "<img src='gold.png' alt='Gold medal' height='40' width='40'>";
+			silver.innerHTML = "Silver Medal for Canada! With " +Canadac +" victories!" + "<img src='silver.jpg' alt='Silver medal' height='40' width='40'>";
+		}
+		else
+		{
+			gold.innerHTML = "Gold Medal for USA! With " + USAc +" victory!" + "<img src='gold.png' alt='Gold medal' height='40' width='40'>";
+			silver.innerHTML = "Silver Medal for Canada! With " +Canadac +" victories!" + "<img src='silver.jpg' alt='Silver medal' height='40' width='40'>";	
+		}
+		
+		head.innerHTML = "<img src='americanflag.jpg' alt='USA' height='40' width='40'> Olympic Tic-Tac-Toe <img src='americanflag.jpg' alt='USA' height='40' width='40'>";
+	}
+	else
+	{
+		if(Canadac > 1 && USAc == 1)
+		{
+			gold.innerHTML = "Gold Medal for Canada! With " + Canadac +" victories!" + "<img src='gold.png' alt='Gold medal' height='40' width='40'>";
+			silver.innerHTML = "Silver Medal for USA! With " +USAc +" victory!" + "<img src='silver.jpg' alt='Silver medal' height='40' width='40'>";
+		}
+		else if(Canadac > 1 && USAc > 1)
+		{
+			gold.innerHTML = "Gold Medal for Canada! With " + Canadac +" victories!" + "<img src='gold.png' alt='Gold medal' height='40' width='40'>";
+			silver.innerHTML = "Silver Medal for USA! With " +USAc +" victories!" + "<img src='silver.jpg' alt='Silver medal' height='40' width='40'>";
+		}
+		else if(Canadac == 1 && USAc == 1)
+		{
+			gold.innerHTML = "Gold Medal for Canada! With " + Canadac +" victory!" + "<img src='gold.png' alt='Gold medal' height='40' width='40'>";
+			silver.innerHTML = "Silver Medal for USA! With " +USAc +" victory!" + "<img src='silver.jpg' alt='Silver medal' height='40' width='40'>";
+		}
+		else if(Canadac > 1 && USAc == 0)
+		{
+			gold.innerHTML = "Gold Medal for Canada! With " + Canadac +" victories!" + "<img src='gold.png' alt='Gold medal' height='40' width='40'>";
+			silver.innerHTML = "Silver Medal for USA! With " +USAc +" victories!" + "<img src='silver.jpg' alt='Silver medal' height='40' width='40'>";
+		}
+		else
+		{
+			gold.innerHTML = "Gold Medal for Canada! With " + Canadac +" victory!" + "<img src='gold.png' alt='Gold medal' height='40' width='40'>";
+			silver.innerHTML = "Silver Medal for USA! With " +USAc +" victories!" + "<img src='silver.jpg' alt='Silver medal' height='40' width='40'>";
+		}
+		
+		head.innerHTML = "<img src='canadianflag.jpg' alt='Canada' height='40' width='40'> Olympic Tic-Tac-Toe <img src='canadianflag.jpg' alt='Canada' height='40' width='40'>";
 	}
 
+
 }
+
+function resetgame()
+{
+	var buttons = ["0","1","2","3","4","5","6","7","8"];
+	for(var counter = 0; counter < buttons.length; counter++)
+	{
+		var currentbutton = document.getElementById(buttons[counter]);
+		currentbutton.innerHTML = "";
+	}
+}
+
+
 
